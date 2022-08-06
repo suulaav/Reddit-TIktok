f03b130f801f192efed840a152ae7888bc26598e
Sulav <sulav@aricalot.com>
2022-08-06 19:40:34 +0545

    added passenger_wsgi.py

--- app.py
+++ app.py
@@ -3,20 +3,25 @@ import os
 from flask import Flask, send_file
 from service.service_impl import start
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/make-video/<string:subreddit>")
+@application.route("/make-video/<string:subreddit>")
 def make_video(subreddit):
     return start(subreddit)
 
 
-@app.route("/get-video")
+@application.route("/get-video")
 def get_video():
     dir_path = "media/final/"
     res = os.listdir(dir_path)
     return send_file(dir_path + res[0], mimetype='video/mp4')
 
 
+@application.route("/")
+def ping():
+    return "success"
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
